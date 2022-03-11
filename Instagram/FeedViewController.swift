1d90f71304ee09bdb0e60287ac8b6c11e5b97ba6
Sabrina Chen <chensabrina45@gmail.com>
2022-03-11 18:49:55 -0500

    Initial Commit

---
+++ Instagram/FeedViewController.swift
@@ -0,0 +1,113 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Sabrina Chen on 3/11/22.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    let myRefreshControl = UIRefreshControl()
+    let postsAmount = 20
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        myRefreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        // Do any additional setup after loading the view.
+    }
+    @objc func loadPosts(){
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = postsAmount
+        query.order(byDescending: "createdAt")
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadPosts()
+//        let query = PFQuery(className: "Posts")
+//        query.includeKey("author")
+//        query.limit = 20
+//        query.order(byDescending: "createdAt")
+//
+//        query.findObjectsInBackground { (posts, error) in
+//            if posts != nil {
+//                self.posts = posts!
+//                self.tableView.reloadData()
+//            }
+//        }
+    }
+    
+    func loadMorePosts(){
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = postsAmount + 20
+        query.order(byDescending: "createdAt")
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count{
+            loadMorePosts()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af.setImage(withURL: url)
+        return cell
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Sabrina Chen on 3/11/22.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
